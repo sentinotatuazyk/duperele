ad53e17d75b3a624801334c46980523a6e97ea04
Konrad <konrad@toborek.com>
2025-05-04 01:39:12 +0200

    Skrypt do sprawdzania wydajnosci

--- script.py
+++ script.py
@@ -1,12 +1,21 @@
 import time
 import random
 lista = []
+print("Tworzenie listy")
+start0 = time.time()
 for i in range(10000000):
     liczba =  random.randint(0, 999999999)
     lista.append(liczba)
-start = time.time()
+end0 = time.time()
+print("#################")
+print("Stortowanie")
+start1 = time.time()
 lista.sort()
-end = time.time()
+end1 = time.time()
 
-czas = end - start
-print("Czas sortowania: ", czas)
+czas = end0 - start0 + end1 - start1
+print("#################")
+print("Czas tworzenia listy: ", end0 - start0)
+print("Czas sortowania: ", end1 - start1)
+print("Czas ogolny: ", czas)
+
